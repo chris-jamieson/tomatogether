b3839dd9ee92ee2a21c265fe1df49a78d7489ade
Chris Jamieson <jamiesoncj@gmail.com>
2015-12-10 21:14:03 +0000

    added following status hook

--- server/collectionHooks.js
+++ server/collectionHooks.js
@@ -34,14 +34,56 @@ Timers.after.update(function (userId, doc, fieldNames, modifier, options) {
 			};
 
 			Timers.update( docToUpdate._id, { $set: $set }, function ( error, result ) {
-            if ( error ){
-                console.log ( error );
-            }
+                if ( error ){
+                    console.log ( error );
+                }
 
-            if ( result ){
-                console.log ( result );
-            }
-        } );
+                if ( result ){
+                    console.log ( result );
+                }
+            } );
 		}
     });
+});
+
+Meteor.users.after.update(function (userId, doc, fieldNames, modifier, options) {
+    // see if this user has updated their following field
+    if( _.contains(fieldNames, 'following')) {
+        if ( doc.following && doc.following !== '' ) {
+            // stop this user's latest timer
+            var myTimers = Timers.find( { createdBy: doc._id }, { sort: { createdAt: -1 }, limit: 1 } ).fetch();
+            if (myTimers.length > 0) {
+                Timers.update( myTimers[0]._id, { $set: { status: 'stopped' } }, function ( error, result ) {
+                    if ( error ){
+                        console.log ( error );
+                    }
+
+                    if ( result ){
+                        console.log ( result );
+                    }
+                } );
+            }
+
+            // find the leader's recent timer and clone it
+            var leaderId = doc.following;
+            var timers = Timers.find( { createdBy: leaderId }, { sort: { createdAt: -1 }, limit: 1 } ).fetch();
+            if ( timers.length > 0 ) {
+                // change the user ID and remove the doc ID
+                var docToInsert = timers[0];
+                delete docToInsert._id;
+                docToInsert.createdBy = doc._id;
+                // insert a cloned timer
+                Timers.insert( docToInsert, function ( error, result ) {
+                    if ( error ){
+                        console.log ( error );
+                    }
+
+                    if ( result ){
+                        console.log ( result );
+                    }
+                } );
+            }
+
+        }
+    }
 });
